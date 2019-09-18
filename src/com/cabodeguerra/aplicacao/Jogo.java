ce84541c450a8cfe3d4c617aa55309177fef03b2
Vagner da Silva <vagnerdasilva94@gmail.com>
2019-09-18 12:24:15 -0300

    Calculo provisorio das formulas

--- src/com/cabodeguerra/aplicacao/Jogo.java
+++ src/com/cabodeguerra/aplicacao/Jogo.java
@@ -5,7 +5,7 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class Jogo {
-	
+    
     public static void main(String args[]) {
 
         Locale.setDefault(Locale.US);
@@ -15,9 +15,10 @@ public class Jogo {
 
         System.out.println("\n\n");
         if(opcao == 'y' || opcao == 'Y') {
-            Equipe equipe1;
-            equipe1 = new Equipe();
-            Equipe equipe2 = new Equipe();
+            System.out.print("Digite o nome da Equipe 1: ");
+            Equipe equipe1 = new Equipe(nomeEquipe());
+            System.out.print("Digite o nome da Equipe 2: ");
+            Equipe equipe2 = new Equipe(nomeEquipe());
             partida(equipe1, equipe2);
         }
         else{
@@ -38,7 +39,7 @@ public class Jogo {
         System.out.println(">>>REGRAS<<<");
 
         System.out.println("- O jogo possui dois parametros: forca e resistencia;");
-        System.out.println("- O jogador possui 10 pontos por rodada para alocar nos parametros");
+        System.out.println("- O jogador possui 10 pontos por rodada para alocar nos parametros;");
         System.out.println("- Cada um dos parametros podem ter ate 10 pontos alocados;");
         System.out.println("- Cada jogador possui 30 pontos no seu campo;");
         System.out.println("- Vence o jogador que fizer o adiversario chegar na linha central (perder 30 pontos).");
@@ -64,25 +65,71 @@ public class Jogo {
     }
 
     private static void partida(Equipe equipe1, Equipe equipe2) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        
         int tamanho = 76;
         int demarcacao = (tamanho / 2);
-        equipe1. = 30;
+        int campoEquipe1 = 30;
         int campoEquipe2 = 30;
+        int forcaEquipe1;
+        int forcaEquipe2;
+        boolean rodando = false;
+        String[] nomeEquipes = new String[tamanho];
         String[] campo = new String[tamanho];
 
-        renderizarTela(campo, demarcacao, campoEquipe1, campoEquipe2);
+        renderizarTela(campo, nomeEquipes, demarcacao, campoEquipe1, campoEquipe2, equipe1, equipe2);
         System.out.println("\n");
         
+        while(!rodando){
+            System.out.print("Equipe 1: quanto deseja alocar em forca? ");
+            forcaEquipe1 = sc.nextInt();
+            
+            System.out.print("Equipe 2: quanto deseja alocar em forca? ");
+            forcaEquipe2 = sc.nextInt();
+
+            int vencedorRodada = calculoRodada(forcaEquipe1, forcaEquipe2);
+            
+            if(vencedorRodada == 1){
+                campoEquipe1 += forcaEquipe1 - forcaEquipe2;
+                campoEquipe2 -= forcaEquipe1 - forcaEquipe2;
+                System.out.println("\n\nEquipe " + equipe1.getNome() + " aplicou mais forca, ganhando mais terreno!");
+            }
+            else if(vencedorRodada == 2){
+                campoEquipe1 -= forcaEquipe2 - forcaEquipe1;
+                campoEquipe2 += forcaEquipe2 - forcaEquipe1;
+                System.out.println("\n\nEquipe " + equipe2.getNome() + " aplicou mais forca, ganhando mais terreno!");
+            }
+            else{
+                System.out.println("\n\nEmpate! Chegamos em um impasse de forcas!");
+            }
+            
+            renderizarTela(campo, nomeEquipes, demarcacao, campoEquipe1, campoEquipe2, equipe1, equipe2);
+            System.out.println("\n");
+            
+            if(campoEquipe1 <= 0 || campoEquipe2 <= 0){
+                //if()
+                rodando = true;
+            }
+        }
+    }
+    
+    private static int calculoRodada(int forcaEquipe1, int forcaEquipe2){
+        if(forcaEquipe1 > forcaEquipe2){
+            return 1;
+        }
+        else if(forcaEquipe2 > forcaEquipe1){
+            return 2;
+        }
         
-
-
+        return 0;
     }
 
     private static void fimJogo() {
         System.out.println("### Fim do jogo! ###\n");
     }
 
-    private static void renderizarTela(String[] campo, int demarcacao, int campoEquipe1, int campoEquipe2) {
+    private static void renderizarTela(String[] campo, String[] nomeEquipes, int demarcacao, int campoEquipe1, int campoEquipe2, Equipe equipe1, Equipe equipe2) {
         for(int i = 0; i < campo.length; i++) {
             if(i == demarcacao) {
                 campo[i] = "|";
@@ -125,6 +172,26 @@ public class Jogo {
                 campo[i] = " ";
             } 
         }
+        
+        for(int i = 0; i < nomeEquipes.length; i++){
+            if(i == 0){
+                nomeEquipes[i] = equipe1.getNome();
+            }
+            else if(i == nomeEquipes.length - 5){
+                nomeEquipes[i] = equipe2.getNome();
+            }
+            else{
+                nomeEquipes[i] = " ";
+            }
+        }
+        
+        System.out.println("\n");
+        
+        for(String n : nomeEquipes) {
+            System.out.print(n);
+        }
+        
+        System.out.println("\n");
 
         for(String r : campo) {
             System.out.print(r);
@@ -133,8 +200,11 @@ public class Jogo {
         System.out.println("\n");
     }
     
-    public void setNome(String nome){
-        nome 
+    public static String nomeEquipe(){
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        
+        return sc.nextLine();
     }
 	
 }
